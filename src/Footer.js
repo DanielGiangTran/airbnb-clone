496af203606e084c9bbee46947b8a76f7e272b0f
DanielGiangTran <trantruonggiang7a@gmail.com>
2021-04-04 01:23:21 +1300

    first-verion

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+function Footer() {
+  return (
+    <div className="footer">
+      <p>© 2021 Airbnb clone! No rights reserved - this is just a demo</p>
+      <p> Privacy · Terms · Sitemap · Company Details </p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header__icon"
+          src="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-logo.jpg"
+          alt=""
+        />
+      </Link>
+
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
